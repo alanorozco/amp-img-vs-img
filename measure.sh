0afebf1f6bc353c8ac9e19abeb962dd9d553279b
Alan Orozco <alan@orozco.xyz>
2020-10-13 17:41:09 -0700

    uncomment

--- measure.sh
+++ measure.sh
@@ -4,23 +4,15 @@ cd output
 
 for D in *; do
   if [ -d "${D}" ]; then
+    cat "${D}/url.txt"
+    echo "\n"
     npx tach \
+      --timeout=1 \
       --browser=chrome-headless \
       --measure=global \
       --measurement-expression="window.__vitals__.lcp" \
       --json-file="${D}/result-lcp.json" \
-      "${D}/amp-img.html" \
-      "${D}/img.html"
-  fi
-done
-
-for D in *; do
-  if [ -d "${D}" ]; then
-    npx tach \
-      --browser=chrome-headless \
-      --measure=global \
-      --measurement-expression="window.__vitals__.cls" \
-      --json-file="${D}/result-cls.json" \
+      --csv-file="${D}/result-lcp.csv" \
       "${D}/amp-img.html" \
       "${D}/img.html"
   fi
